fd980c66234d1d1167b640d37899fce6a2050f90
arebac <j.cabreraortiz@outlook.com>
2024-03-06 17:10:38 -0400

    Starting Pizza App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Pizza from "../src/components/Pizza";
+
+function App() {
+  return (
+    <div>
+      <Pizza />
+      <Pizza />
+      <Pizza />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Pizza.jsx
@@ -0,0 +1,12 @@
+const Pizza = () => {
+  return (
+    <div>
+    <img src="public\pizzas\spinaci.jpg"></img>
+      <h2>Pizza Spinaci</h2>
+      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+      
+    </div>
+  );
+};
+
+export default Pizza;
